588038a5c8b4ed19052d868812d84ef4f0de870c
DameHetfield9803 <you@example.com>
2024-04-20 14:51:24 +0800

    v 0.1.0 starts now

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,28 @@
 'use client'
 import Image from "next/image";
+import { TypeAnimation } from "react-type-animation"
 import ThemeSwitch from "@/app/themeswitch";
 import "./globals.css"
 export default function Home() {
   return (
-      <ul className={"root container flex text-center steps step p-8"}>
-          <li>
-              <>
-                <ul className={"container inline-flex space-x-4 step step-primary justify-start align-middle"} data-content={""}>
-                    <li><strong>Hello world</strong></li>
-                    <li><ThemeSwitch/></li>
-                </ul>
-              </>
-          </li>
-
-      </ul>
+      <div className={"container text-center subpixel-antialiased flex-auto mx-auto rounded-md p-10 max-w-screen-xl min-h-screen mix-blend-plus-darker"}>
+          <div className={"text-center container mx-auto flex-auto p-4 min-h-dvh max-w-screen-lg outline-cyan-900 outline rounded-xl subpixel-antialiased"}>
+              <nav className={"mb-2 rounded-lg"}>
+                  <ul>
+                      <li className={"justify-end"}><ThemeSwitch/></li>
+                      <li></li>
+                  </ul>
+              </nav>
+              <h1 className={"text-[4.25em] mb-2"}><strong className={"text-blue-500"}>Welcome to <span className={"text-rose-500"}>
+                  <TypeAnimation
+                      sequence={
+                      [
+                          "xyz",
+                          1000
+                      ]
+                  } repeat={Infinity}/></span></strong></h1>
+              <p className={"text-xl text-pretty text-lime-300"}>To be continued...</p>
+          </div>
+      </div>
   );
 }
